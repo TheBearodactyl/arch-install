d8bff1a22eabbd3784381d9bbde7cfa8305a13d7
TheBearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2023-02-06 04:07:11 +0000

    Create post-chroot.sh

---
+++ Arch/post-chroot.sh
@@ -0,0 +1,20 @@
+nano /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+echo thebearodactyluwu > /etc/hostname
+touch /etc/hosts
+nano /etc/hosts
+passwd
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --bootloader-id="BEHOLD PERRY THE PLATYPUS!!! THE ARCH-INATOR 300!!!!!!!!" --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S sudo
+useradd -m thebearodactyluwu
+passwd thebearodactyluwu
+usermod -aG wheel,audio,video,storage team
+EDITOR=nano visudo
+nano /etc/pacman.conf
+pacman -S xorg networkmanager gnome plasma plasma-wayland-session git chromium
+systemctl enable gdm NetworkManager
+exit
