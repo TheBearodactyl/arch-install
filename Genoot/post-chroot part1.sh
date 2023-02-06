64e683a983481e22060b2796857a28ba49ff8849
TheBearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2023-02-06 04:46:39 +0000

    Delete Genoot directory

--- Genoot/post-chroot part1.sh
+++
@@ -1,5 +0,0 @@
-source /etc/profile
-export PS1="(chroot) $PS1"
-emerge-webrsync
-emerge --sync
-eselect profile list
--- Genoot/post-chroot part3.sh
+++
@@ -1,1 +0,0 @@
-env-update && source /etc/profile && export PS1="(chroot) $PS1"
