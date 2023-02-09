6cadb2e08bbb32a904dd5f315513ebf84f92be64
TheBearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2023-02-08 18:58:17 -0600

    Delete Arch directory

--- Arch/post-install 2.sh
+++
@@ -1,4 +0,0 @@
-cd /home/thebearodactyluwu
-git clone https://github.com/Axarva/dotfiles-2.0
-cd dotfiles-2.0
-sudo chmod +x install-on-arch.sh
--- Arch/post-install.sh
+++
@@ -1,7 +0,0 @@
-sudo -s
-pacman -Syu
-pacman -S virtualbox
-touch /etc/modules-load.d/virtualbox.conf
-echo vboxdrv > /etc/modules-load.d/virtualbox.conf
-sudo usermod -aG vboxusers $(whoami)
-reboot now
